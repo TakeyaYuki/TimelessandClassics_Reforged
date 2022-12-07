ceb2d045172abf88061302614ade584c68aacd48
Clumsy Alien <therealclumsyalien@gmail.com>
2022-12-06 20:13:09 -0500

    add extra

---
+++ src/main/java/com/tac/guns/client/TODO.md
@@ -0,0 +1,3 @@
+**Generic special model loader**
+
+- Special models is separate pieces of a single model, these separations allow us to direct animations to certain weapon pieces, along with rendering attachments part by part
--- src/main/java/com/tac/guns/client/render/gun/model/m1911_animation.java
+++ src/main/java/com/tac/guns/client/render/gun/model/m1911_animation.java
@@ -2,6 +2,7 @@ package com.tac.guns.client.render.gun.model;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.tac.guns.Config;
+import com.tac.guns.client.SpecialModel;
 import com.tac.guns.client.SpecialModels;
 import com.tac.guns.client.handler.GunRenderingHandler;
 import com.tac.guns.client.handler.ReloadHandler;
@@ -36,6 +37,11 @@ import net.minecraft.util.math.vector.Vector3f;
  */
 public class m1911_animation implements IOverrideModel {
 
+    private final SpecialModel M1911 = new SpecialModel("m1911");
+    private final SpecialModel M1911_SLIDE = new SpecialModel("m1911_slide");
+    private final SpecialModel M1911_STANDARD_MAG = new SpecialModel("m1911_standard_mag");
+    private final SpecialModel M1911_LONG_MAG = new SpecialModel("m1911_long_mag");
+
     //The render method, similar to what is in DartEntity. We can render the item
     @Override
     public void render(float v, ItemCameraTransforms.TransformType transformType, ItemStack stack, ItemStack parent, LivingEntity entity, MatrixStack matrices, IRenderTypeBuffer renderBuffer, int light, int overlay)
@@ -43,24 +49,24 @@ public class m1911_animation implements IOverrideModel {
         M1911AnimationController controller = M1911AnimationController.getInstance();
         matrices.push();
         {
-            controller.applySpecialModelTransform(SpecialModels.M1911.getModel(),M1911AnimationController.INDEX_BODY,transformType,matrices);
-            if (Gun.getAttachment(IAttachment.Type.PISTOL_BARREL, stack).getItem() == ModItems.PISTOL_SILENCER.get()) {
+            controller.applySpecialModelTransform(M1911.getModel(),M1911AnimationController.INDEX_BODY,transformType,matrices);
+            /*if (Gun.getAttachment(IAttachment.Type.PISTOL_BARREL, stack).getItem() == ModItems.PISTOL_SILENCER.get()) {
                 matrices.push();
                 matrices.translate(0, 0, -0.0475);
-                RenderUtil.renderModel(SpecialModels.M1911_SUPPRESSOR.getModel(), stack, matrices, renderBuffer, light, overlay);
+                RenderUtil.renderModel(M1911_SUPPRESSOR.getModel(), stack, matrices, renderBuffer, light, overlay);
                 matrices.translate(0, 0, 0.0475);
                 matrices.pop();
-            }
-            RenderUtil.renderModel(SpecialModels.M1911.getModel(), stack, matrices, renderBuffer, light, overlay);
+            }*/
+            RenderUtil.renderModel(M1911.getModel(), stack, matrices, renderBuffer, light, overlay);
         }
         matrices.pop();
         matrices.push();
         {
-            controller.applySpecialModelTransform(SpecialModels.M1911.getModel(),M1911AnimationController.INDEX_MAG,transformType,matrices);
+            controller.applySpecialModelTransform(M1911.getModel(),M1911AnimationController.INDEX_MAG,transformType,matrices);
             if (EnchantmentHelper.getEnchantmentLevel(ModEnchantments.OVER_CAPACITY.get(), stack) > 0) {
-                RenderUtil.renderModel(SpecialModels.M1911_LONG_MAG.getModel(), stack, matrices, renderBuffer, light, overlay);
+                RenderUtil.renderModel(M1911_LONG_MAG.getModel(), stack, matrices, renderBuffer, light, overlay);
             } else {
-                RenderUtil.renderModel(SpecialModels.M1911_STANDARD_MAG.getModel(), stack, matrices, renderBuffer, light, overlay);
+                RenderUtil.renderModel(M1911_STANDARD_MAG.getModel(), stack, matrices, renderBuffer, light, overlay);
             }
         }
         matrices.pop();
@@ -69,7 +75,7 @@ public class m1911_animation implements IOverrideModel {
         float cooldownOg = tracker.getCooldown(stack.getItem(), Minecraft.getInstance().getRenderPartialTicks()); // getRenderPartialTicks()); // getCooldown(stack.getItem(), Minecraft.getInstance().getFrameTime());
         matrices.push();
         {
-            controller.applySpecialModelTransform(SpecialModels.M1911.getModel(),M1911AnimationController.INDEX_SLIDE,transformType,matrices);
+            controller.applySpecialModelTransform(M1911.getModel(),M1911AnimationController.INDEX_SLIDE,transformType,matrices);
             AnimationMeta reloadEmpty = controller.getAnimationFromLabel(GunAnimationController.AnimationLabel.RELOAD_EMPTY);
             boolean shouldOffset = reloadEmpty != null && reloadEmpty.equals(controller.getPreviousAnimation()) && controller.isAnimationRunning();
 
@@ -85,7 +91,7 @@ public class m1911_animation implements IOverrideModel {
             }
 
             matrices.translate(0.00, 0.0, -0.008);
-            RenderUtil.renderModel(SpecialModels.M1911_SLIDE.getModel(), stack, matrices, renderBuffer, light, overlay);
+            RenderUtil.renderModel(M1911_SLIDE.getModel(), stack, matrices, renderBuffer, light, overlay);
         }
         matrices.pop();
 
